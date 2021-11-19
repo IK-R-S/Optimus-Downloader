5717ebf1f3bd28e3dca26d4aeef9559e8715652c
IK-R-S <73291742+IK-R-S@users.noreply.github.com>
2021-11-18 21:25:17 -0300

    Update video.py

--- components/video.py
+++ components/video.py
@@ -10,7 +10,7 @@ def video():
     source = video.streams.get_highest_resolution()
     print(f'\033[1;41m [{video.title} -- {video.author}]\033[0;0m')
     print('\033[1;31m \n Fazendo o download, espere um pouco...')
-    destino = source.download(output_path='/home/krs/programação/projetos/Pytube_Downloader/downloads')
+    destino = source.download(output_path='SUA PASTA AQUI')
     base, ext = os.path.splitext(destino)
     new_file = base + '.mp4'
     os.rename(destino, new_file)
